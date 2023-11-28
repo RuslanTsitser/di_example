4470fa1b5cc5d7950865008352a9714ec89ac1fa
RuslanTsitser <cicer_ruslan@mail.ru>
2023-11-28 19:04:59 +0300

    base

--- lib/core/services/api_service/api_service.dart
+++ lib/core/services/api_service/api_service.dart
@@ -1,13 +1,29 @@
 import 'package:di_example/core/constants/app_const.dart';
+import 'package:di_example/core/logging/dio_logger_mixin.dart';
 import 'package:dio/dio.dart';
 
 abstract class ApiService {
   Dio get rickAndMortyDio;
 }
 
-class ApiServiceImpl implements ApiService {
+class ApiServiceImpl with DioLoggerMixin implements ApiService {
   @override
   Dio get rickAndMortyDio => Dio(
         BaseOptions(baseUrl: AppConst.rickAndMortyUrl),
-      );
+      )..interceptors.add(
+          InterceptorsWrapper(
+            onRequest: (options, handler) {
+              onRequest(options);
+              return handler.next(options);
+            },
+            onResponse: (response, handler) {
+              onResponse(response);
+              return handler.next(response);
+            },
+            onError: (e, handler) {
+              onError(e);
+              return handler.next(e);
+            },
+          ),
+        );
 }
